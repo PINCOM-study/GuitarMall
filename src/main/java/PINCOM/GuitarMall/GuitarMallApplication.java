e92f46ba62c8f3e9f320f6250c7cef9390dd0069
leeju0 <92801522+leeju0@users.noreply.github.com>
2023-11-15 13:13:03 +0900

    Initial commit

---
+++ src/main/java/PINCOM/GuitarMall/GuitarMallApplication.java
@@ -0,0 +1,13 @@
+package PINCOM.GuitarMall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuitarMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuitarMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PINCOM/GuitarMall/GuitarMallApplicationTests.java
@@ -0,0 +1,13 @@
+package PINCOM.GuitarMall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuitarMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
