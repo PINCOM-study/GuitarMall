92d4d16be583ff4e64e4e339bbdcafcc53e5aa87
ImKEISS <kss5122@naver.com>
2023-11-20 03:40:03 +0900

    회원가입 및 로그인 프로토타입 추가

---
+++ src/main/java/PINCOM/GuitarMall/member/controller/MemberController.java
@@ -0,0 +1,48 @@
+package PINCOM.GuitarMall.member.controller;
+
+import PINCOM.GuitarMall.member.dto.MemberDto;
+import PINCOM.GuitarMall.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("")
+    public String homeGET() {
+        return "main";
+    }
+
+    @GetMapping("login")
+    public void loginGET() {}
+
+    @PostMapping("login")
+    public String loginPOST(MemberDto memberDto, RedirectAttributes re) {
+
+        re.addFlashAttribute("username", memberDto.getUsername());
+
+        String message = memberService.login(memberDto);
+        re.addFlashAttribute("message", message);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("join")
+    public void joinGET() {}
+
+    @PostMapping("join")
+    public String joinPOST(MemberDto memberDto, RedirectAttributes re) {
+        System.out.println("memberDto = " + memberDto);
+        String username = memberService.join(memberDto);
+
+        re.addFlashAttribute("username", username);
+
+        return "redirect:/login";
+    }
+}
