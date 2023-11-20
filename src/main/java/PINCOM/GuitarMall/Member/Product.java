92c31747972ff6c8fb9f0d7b606bfaba77d487f5
leeju0 <92801522+leeju0@users.noreply.github.com>
2023-11-20 15:50:26 +0900

    기타 상품 domain

---
+++ src/main/java/PINCOM/GuitarMall/Member/Product.java
@@ -0,0 +1,79 @@
+package PINCOM.GuitarMall.Member;
+
+public class Product {
+
+    private String productId;
+    private String name;
+    private String category;
+    private int Price;
+    private String brand;
+    private long unitsInStock;
+    private String releaseDate;
+    private String national;
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getPrice() {
+        return Price;
+    }
+
+    public void setPrice(int price) {
+        Price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public long getUnitsInStock() {
+        return unitsInStock;
+    }
+
+    public void setUnitsInStock(long unitsInStock) {
+        this.unitsInStock = unitsInStock;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getNational() {
+        return national;
+    }
+
+    public void setNational(String national) {
+        this.national = national;
+    }
+
+
+}
