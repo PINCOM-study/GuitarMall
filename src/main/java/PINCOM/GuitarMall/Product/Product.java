aa060deb1e3e0190435ec72bada8c641e2a722b4
ImKEISS <kss5122@naver.com>
2023-11-20 20:54:14 +0900

    Product 패키지 분리 및 롬복 Getter, Setter 사용

---
+++ src/main/java/PINCOM/GuitarMall/Product/Product.java
@@ -0,0 +1,20 @@
+package PINCOM.GuitarMall.Product;
+
+import lombok.Getter;
+import lombok.Setter;
+
+//상품 도메인
+@Getter
+@Setter
+public class Product {
+
+    private String productId;
+    private String name;
+    private String category;
+    private int Price;
+    private String brand;
+    private long unitsInStock;
+    private String releaseDate;
+    private String national;
+
+}
