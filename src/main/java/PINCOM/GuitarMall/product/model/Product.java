dc9ee67b2dc7553b0711f0f0ba47144a99b0a3ea
leeju0 <92801522+leeju0@users.noreply.github.com>
2023-11-29 16:25:12 +0900

    상품엔티티 @Builder, @Column 어노테이션 추가

--- src/main/java/PINCOM/GuitarMall/product/model/Product.java
+++ src/main/java/PINCOM/GuitarMall/product/model/Product.java
@@ -12,17 +12,31 @@ import lombok.*;
 @DiscriminatorColumn(name ="DTYPE")
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long productId;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private int price;
+
+    @Column(nullable = false)
     private String brand;
+
+    @Column(nullable = false)
     private long unitsInStock;
+
+    @Column(nullable = false)
     private String releaseDate;
+
+    @Column(nullable = false)
     private String national;
 
+    @Column(nullable = false)
     private String category;
 }
 
