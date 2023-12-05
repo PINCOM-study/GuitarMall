9c764754d3e71d7f642bbccca9525bcd46cdeb7f
ImKEISS <kss5122@naver.com>
2023-12-05 14:08:29 +0900

    로그인 실패 메시지 및 로그아웃 구현

--- src/main/java/PINCOM/GuitarMall/member/controller/MemberController.java
+++ src/main/java/PINCOM/GuitarMall/member/controller/MemberController.java
@@ -40,20 +40,31 @@ public class MemberController {
         String message = memberService.login(memberDto);
         if (message.equals("username")) {   // 아이디 오류
             message = "아이디가 없습니다";
+            redirectURL="/login?redirectURL=" + redirectURL;    // 사용자가 원래 가려고 했던 URL 저장
         }
         else if (message.equals("password")) {  // 비밀번호 오류
             message = "비밀번호가 틀립니다";
+            re.addFlashAttribute("username", memberDto.getUsername());  // 입력 폼에 아이디 유지
+            redirectURL="/login?redirectURL=" + redirectURL;    // 사용자가 원래 가려고 했던 URL 저장
         }
         else {  // 로그인 성공
             message = "로그인 성공!";
             HttpSession session = req.getSession();
-            session.setAttribute("loginInfo", memberDto.getUsername());
+            session.setAttribute("loginInfo", memberDto.getUsername()); // 세션에 로그인한 유저 닉네임 등록
         }
 
         // 로그인 성공 여부 메세지 전송
         re.addFlashAttribute("message", message);
 
         return "redirect:" + redirectURL;
+    }
+
+    @PostMapping("logout")
+    public String logoutPost(HttpServletRequest req) {
+        HttpSession session = req.getSession();
+        session.removeAttribute("loginInfo");
+
+        return "redirect:/login";
     }
 
     @GetMapping("join")
